5134337809851bbf4826b29d924927635fbe9c4a
Warrensy <74967142+Warrensy@users.noreply.github.com>
2021-04-15 20:42:02 +0200

    Formatierung

--- Arcade_Games/Arcade_Games.cpp
+++ Arcade_Games/Arcade_Games.cpp
@@ -1,5 +1,10 @@
-// Arcade_Games.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// FH Technikum Wien
+// IT-Projekt SS2021
+// C2
+// Arcade_Games
+// Groupe 20
+// Mentor: Petz Markus
+// Dorian Fetty, Florian Lederer, Simon Lercher, Johannes Ratgeb, Patrick Polley
 
 #include <iostream>
 
@@ -11,13 +16,10 @@ int main()
     std::cout << "Dorian\n";
 }
 
+// Tips for Getting Started:
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
+// In the future, to open this project again, go to File > Open > Project and select the .sln file
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
