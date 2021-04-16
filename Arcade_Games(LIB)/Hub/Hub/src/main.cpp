1e27311d7ae168cbe6da63fb06dbb05879fb7c18
Warrensy <74967142+Warrensy@users.noreply.github.com>
2021-04-16 22:08:29 +0200

    Hub menu prototyp
    Hab ein Menü eingerichtet.

--- Arcade_Games(LIB)/Hub/Hub/src/main.cpp
+++ Arcade_Games(LIB)/Hub/Hub/src/main.cpp
@@ -3,27 +3,54 @@
 
 int main()
 {
-	oasenCrawler::PrintMessage();
-	oasenCrawler::PlayOasenCrawler();
-	std::cout << "Fertig";
+	//oasenCrawler::PrintMessage();
+	//oasenCrawler::PlayOasenCrawler();
+	//std::cout << "Fertig";
 	
-	//int input = 0;
-	//system("cls");
-	//char input;
-
-	//std::cout << "--------------------";
-	//std::cout << std::endl << "bla";
-	//std::cout << std::endl << "--------------------";
-	//accept = input;
-	//input? == x
-	//std::cout << std::endl << "bla2";
-	//std::cout << std::endl << "bla3";
-	//std::cout << std::endl << "bla 4";
-	//input = getchar();
-	//if (accept == 1 && input == 'x')
-	//{
-	//	oasencrawler::playoasencrawler;
-	//}
-
+	int selected = 0;
+	char confirm = 'z';
+	while (selected < 3 && confirm != 'q')
+	{
+		system("cls");
+		std::cout << " WELCOME BACK" << std::endl;
+		std::cout << std::endl << "   Select Game: " << std::endl;
+		if (selected <= 0)
+		{
+			selected = 0;
+			std::cout << std::endl << "     Oasen_Crawler <-";
+			std::cout << std::endl << "     Deep_Miner";
+			std::cout << std::endl << "     Fight_Club" << std::endl;
+		}
+		else if (selected == 1)
+		{
+			std::cout << std::endl << "     Oasen_Crawler";
+			std::cout << std::endl << "     Deep_Miner <-";
+			std::cout << std::endl << "     Fight_Club" << std::endl;
+		}
+		else if (selected >= 2)
+		{
+			selected = 2;
+			std::cout << std::endl << "     Oasen_Crawler";
+			std::cout << std::endl << "     Deep_Miner";
+			std::cout << std::endl << "     Fight_Club <-";
+		}
+		confirm = getchar();
+		if (confirm == 'y')
+		{
+			if(selected == 0) { oasenCrawler::PlayOasenCrawler(); }
+			if (selected == 1) { std::cout << "**Content will be available soon**"; }
+			if(selected == 2) { std::cout << "**Content will be available soon**"; }
+			confirm = getchar();
+			confirm = getchar();
+		}
+		if (confirm == 's')
+		{
+			selected++;
+		}
+		else if (confirm == 'w')
+		{
+			selected--;
+		}
+	}
 	return 0;
 }
