30f6e10b71b0814fda94c437f854671249822ee0
Warrensy <74967142+Warrensy@users.noreply.github.com>
2021-04-16 16:16:56 +0200

    finished for today
    Eine Grundidee wie die Spieleidee im Hub aussehen könnte.

--- Arcade_Games(LIB)/Hub/Hub/src/main.cpp
+++ Arcade_Games(LIB)/Hub/Hub/src/main.cpp
@@ -6,6 +6,24 @@ int main()
 	oasenCrawler::PrintMessage();
 	oasenCrawler::PlayOasenCrawler();
 	std::cout << "Fertig";
+	
+	//int input = 0;
+	//system("cls");
+	//char input;
+
+	//std::cout << "--------------------";
+	//std::cout << std::endl << "bla";
+	//std::cout << std::endl << "--------------------";
+	//accept = input;
+	//input? == x
+	//std::cout << std::endl << "bla2";
+	//std::cout << std::endl << "bla3";
+	//std::cout << std::endl << "bla 4";
+	//input = getchar();
+	//if (accept == 1 && input == 'x')
+	//{
+	//	oasencrawler::playoasencrawler;
+	//}
 
 	return 0;
 }
